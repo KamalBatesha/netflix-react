f990b4425a89bdf28d9389e989e96c4850da261e
KamalBatesha <bateshakamal@gmail.com>
2024-01-07 03:37:30 +0200

    try16

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,10 @@ function App() {
     <Router>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="netflix-react/" element={<Home />} />
-        <Route path="netflix-react/Login" element={<Login />} />
-        <Route path="netflix-react/Register" element={<Register />} />
-        <Route path="netflix-react/Watch" element={<Watch />} />
+        <Route path="/netflix-react" element={<Home />} />
+        <Route path="/netflix-react/Login" element={<Login />} />
+        <Route path="/netflix-react/Register" element={<Register />} />
+        <Route path="/netflix-react/Watch" element={<Watch />} />
       </Routes>
     </Router>
   );
