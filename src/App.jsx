83522f93dacf5d3f0522a69529f5cebac19051c4
KamalBatesha <bateshakamal@gmail.com>
2024-01-07 03:39:50 +0200

    try17

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/login";
@@ -9,39 +9,39 @@ import Watch from "./pages/watch/watch";
 function App() {
   console.log("lllllllll");
   return (
-    // <div>
-    //   <BrowserRouter>
-    //     {/* <h1>reacr router dom 6</h1>
-    //     <nav>
-    //       <NavLink to="/">Home</NavLink>
-    //       <NavLink to="/Footer">Footer</NavLink>
-    //       <NavLink to="/Header">Header</NavLink>
-    //       <NavLink to="/Books">Books</NavLink>
-    //     </nav> */}
-    //     <Routes>
-    //       <Route index element={<Home />} />
-    //       <Route path="/netflix-react" element={<Home />} />
-    //       <Route path="/netflix-react/Login" element={<Login />} />
-    //       <Route path="/netflix-react/Register" element={<Register />} />
-    //       <Route path="/netflix-react/Watch" element={<Watch />} />
+    <div>
+      <BrowserRouter>
+        {/* <h1>reacr router dom 6</h1>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Footer">Footer</NavLink>
+          <NavLink to="/Header">Header</NavLink>
+          <NavLink to="/Books">Books</NavLink>
+        </nav> */}
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/netflix-react" element={<Home />} />
+          <Route path="/netflix-react/Login" element={<Login />} />
+          <Route path="/netflix-react/Register" element={<Register />} />
+          <Route path="/netflix-react/Watch" element={<Watch />} />
 
-    //       {/* <Route path="*" element={<NotFound />} /> */}
-    //     </Routes>
-    //   </BrowserRouter>
-    // </div>
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
     // <div>
     //   <Home />
     // </div>
 
-    <Router>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/netflix-react" element={<Home />} />
-        <Route path="/netflix-react/Login" element={<Login />} />
-        <Route path="/netflix-react/Register" element={<Register />} />
-        <Route path="/netflix-react/Watch" element={<Watch />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route index element={<Home />} />
+    //     <Route path="/netflix-react" element={<Home />} />
+    //     <Route path="/netflix-react/Login" element={<Login />} />
+    //     <Route path="/netflix-react/Register" element={<Register />} />
+    //     <Route path="/netflix-react/Watch" element={<Watch />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
